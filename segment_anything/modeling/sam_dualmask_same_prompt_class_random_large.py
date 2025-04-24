5603ca88b9a998d45c3b6bd78c07509be7c55127
trnKhanh <tnk29082003@gmail.com>
2025-04-25 03:43:59 +0700

    Fix point prompts

--- segment_anything/modeling/sam_dualmask_same_prompt_class_random_large.py
+++ segment_anything/modeling/sam_dualmask_same_prompt_class_random_large.py
@@ -9,7 +9,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from icecream import ic
-
 from typing import Any, Dict, List, Tuple
 
 from .image_encoder import ImageEncoderViT
